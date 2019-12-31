b2b3e8363ce58d64da4936a987f88104018dabee
Shuai Huang <shuaih@Shuais-MacBook-Pro.local>
2019-12-31 15:26:01 -0800

    profile setup

--- src/main.js
+++ src/main.js
@@ -4,18 +4,21 @@ const main = express()
 const hbs = require('hbs')
 
 
-//set the handlebar
+const partial_dir = __dirname + '/partials'
+const view_dir = __dirname+ '/views'
+hbs.registerPartials(partial_dir)
+
+
 main.set('view engine', 'hbs')
-//set the directory of views
-main.set('views', 'views')
+main.set('views', view_dir)
+main.use(express.static(__dirname ));
 
 //set up the routers
-main.get('/about', (req,res) => {
+main.get('/', (req,res) => {
     res.render('index', {
         name: 'Shuai'
     })
 })
-
 
 main.listen(3000, () => {
     console.log('server is up on port 3000')
