14fb27e9b5411ccdbbd72438ced9d2c32a847be3
Shuai Huang <shhuang9532@gmail.com>
2020-02-29 13:34:13 -0800

    added remote host

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -2,7 +2,7 @@
 
 const run = async () => {
   const mongoose = require('mongoose');
-  await mongoose.connect('mongodb://localhost/shuai_site', {
+  await mongoose.connect('mongodb+srv://shhuang9532:hs13706717787@cluster0-vpaic.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true}
     , (err, client) => {
