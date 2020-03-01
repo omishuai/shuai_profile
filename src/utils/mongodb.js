7dc3eafc4196bdac7627ccfddde95736f224587d
Shuai Huang <shhuang9532@gmail.com>
2020-03-01 10:02:11 -0800

    revised mongodb connection

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,18 +1,30 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const run = async () => {
-  const mongoose = require('mongoose');
-  console.log('mongodb connection: ', process.env.MONGOLAB_URI)
-  await mongoose.connect(process.env.MONGOLAB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true}
-    , (err, client) => {
+  // const mongoose = require('mongoose');
+  // //'mongodb://34.237.64.188:27017/shuai_site'
+  // console.log('mongodb connection: ', process.env.MONGOLAB_URI)
+  // await mongoose.connect(process.env.MONGOLAB_URI, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true}
+  //   , (err, client) => {
+  //     if (err) {
+  //       console.error(err)
+  //       return
+  //     }
+  //     console.log('MongoDB is up running.....')
+    
+  // });
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = process.env.MONGOLAB_URI
+  console.log("string:", uri)
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
       if (err) {
         console.error(err)
         return
       }
       console.log('MongoDB is up running.....')
-    
   });
 }
 run()
