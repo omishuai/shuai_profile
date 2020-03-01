f68e0cd565ed7fb8c2de18b0c98772fe20787e7f
Shuai Huang <shhuang9532@gmail.com>
2020-02-29 19:42:07 -0800

    debugging

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const run = async () => {
   const mongoose = require('mongoose');
+  console.log('mongodb connection: ', process.env.MONGOLAB_URI)
   await mongoose.connect(process.env.MONGOLAB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true}
